8ecedf3880ddd1e39b74482cb01dd032a7dd70c4
Eline Balland <squirel.ine@gmail.com>
2022-04-12 16:42:39 +0200

    fct pour affacer qlq valeurs

---
+++ erase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define dim (9)
+
+// fonction pour créer une nouvelle grille qui sera la grille proposée au joueur, avec quelques valeurs effacée.
+// n_values est le nombre de valeurs effacées
+int ** erase_values(int ** grid, int n_values){
+  srand(time(NULL));
+
+  // on copie la grille complète dans la nouvelle grille
+  int ** play_grid =  malloc(sizeof(int*));
+  for (int i=0;i<dim;i++) {
+ 		play_grid[i]=malloc(sizeof(int));
+    for (int j=0;j<dim;j++){
+      play_grid[i][j] = grid[i][j];
+    }
+ 	}
+  // on génère aléatoirement les coordonnées des valeurs qui seront effacées
+  int x, y;
+  for (int i = 0; i<n_values; i++){
+    x = rand()%dim;
+    y = rand()%dim;
+    play_grid[x][y] = 0;
+  }
+  return play_grid;
+}
