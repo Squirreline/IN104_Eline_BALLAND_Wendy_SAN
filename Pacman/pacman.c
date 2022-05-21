71b7a26b6417cd2c0a00723bd96b4e6b7d468627
Eline Balland <squirel.ine@gmail.com>
2022-05-21 16:39:33 +0200

    nouveau makefile

--- Pacman/pacman.c
+++ Pacman/pacman.c
@@ -107,23 +107,3 @@ void check_lives(struct pacman* PACMAN){
 		}
 	}
 }
-
-
-
-int main(){
-	/*struct ghost allGhosts[NR_GHOSTS];*/
-  	/* Initialisation du pacman*/
-	struct pacman* PACMAN;
-	PACMAN->c.x = 1;
-	PACMAN->c.y = 1;
-	PACMAN->nx = 0;
-	PACMAN->ny = 0;
-	PACMAN->lives = 3;
-	PACMAN->food = 0;
-	struct ghost* all_the_ghosts = malloc(sizeof(struct ghost)*NR_GHOSTS);
-	/*Defining the playing area*/
-<<<<<<< HEAD
-	
-=======
->>>>>>> 65b254f23a4f05d6085d9f8a24f03d0caaf87878
-}
